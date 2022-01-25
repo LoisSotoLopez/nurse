0bae46ad07c979c6f8730859a9ea428532281e4d
Lois Soto <lois.soto@nomasystems.com>
2022-01-25 13:59:03 +0100

    feat(#14): Format

--- lib/nurse_web/live/new_check_live.ex
+++ lib/nurse_web/live/new_check_live.ex
@@ -12,26 +12,28 @@ defmodule NurseWeb.NewCheckLive do
     socket =
       assign(socket,
         yaml_conf: """
-check_name: "my_check"
-check_delay: 10
-connection_timeout: 10
-evaluation_interval: 10
-request_method: GET
-request_body: |
-    { "metric" : "cpu_usage",
-    "unit" : "percent" }
-request_header: |
-    { ContentType: application/json }
-request_hostname: "my_server.com"
-request_port: 8080
-request_scheme: "http"
-response_timeout: 10
-retry_delay: 10
-health_condition: none
-retry_condition: none
-response_condition: none
-""",
-        bad_input_msg: "")
+        check_name: "my_check"
+        check_delay: 10
+        connection_timeout: 10
+        evaluation_interval: 10
+        request_method: GET
+        request_body: |
+            { "metric" : "cpu_usage",
+            "unit" : "percent" }
+        request_header: |
+            { ContentType: application/json }
+        request_hostname: "my_server.com"
+        request_port: 8080
+        request_scheme: "http"
+        response_timeout: 10
+        retry_delay: 10
+        health_condition: none
+        retry_condition: none
+        response_condition: none
+        """,
+        bad_input_msg: ""
+      )
+
     {:ok, socket}
   end
 
@@ -41,36 +43,32 @@ response_condition: none
         socket
       ) do
     yaml_conf = Map.get(params, "yaml_conf")
+
     case ConfigProcessor.process(yaml_conf) do
       {:ok, config} ->
-        {HC.get(config, "check_name"),
-          :starting,
-          {
-            HC.get(config,"request_scheme"),
-            HC.get(config,"request_hostname"),
-            HC.get(config,"request_port")
-          },
-          {
-            HC.get(config,"request_method"),
-            HC.get(config,"request_header"),
-            HC.get(config,"request_body")
-          },
-          HC.get(config,"check_delay"),
-          HC.get(config,"retry_delay"),
-          HC.get(config,"connection_timeout"),
-          HC.get(config,"evaluation_interval"),
-          HC.get(config,"response_condition"),
-          HC.get(config,"response_timeout"),
-          HC.get(config,"health_condition"),
-          HC.get(config,"retry_condition")}
-        |>Healthcheck.from_tuple()
-        |>Client.create
+        {HC.get(config, "check_name"), :starting,
+         {
+           HC.get(config, "request_scheme"),
+           HC.get(config, "request_hostname"),
+           HC.get(config, "request_port")
+         },
+         {
+           HC.get(config, "request_method"),
+           HC.get(config, "request_header"),
+           HC.get(config, "request_body")
+         }, HC.get(config, "check_delay"), HC.get(config, "retry_delay"),
+         HC.get(config, "connection_timeout"), HC.get(config, "evaluation_interval"),
+         HC.get(config, "response_condition"), HC.get(config, "response_timeout"),
+         HC.get(config, "health_condition"), HC.get(config, "retry_condition")}
+        |> Healthcheck.from_tuple()
+        |> Client.create()
+
         {:noreply, redirect(socket, to: "/all-checks")}
+
       {:error, error_string} ->
         socket =
           assign(socket,
-            bad_input_msg:
-              error_string
+            bad_input_msg: error_string
           )
           |> assign(yaml_conf: yaml_conf)
 
--- lib/nurse_web/structs/healthcheck_config.ex
+++ lib/nurse_web/structs/healthcheck_config.ex
@@ -1,44 +1,45 @@
 defmodule NurseWeb.HealthcheckConfig do
-
   alias Nurse
 
   @type key() :: String.t()
 
   @type t() :: %{
-    check_name: Nurse.name(),
-    check_delay: Nurse.check_delay(),
-    connection_timeout: Nurse.connection_timeout(),
-    evaluation_interval: Nurse.evaluation_interval(),
-    request_method: Nurse.method(),
-    request_body: Nurse.body(),
-    request_header: Nurse.headers(),
-    request_hostname: Nurse.hostname(),
-    request_port: Nurse.eport(),
-    request_scheme: Nurse.scheme(),
-    response_timeout: Nurse.response_timeout(),
-    retry_delay: Nurse.retry_delay(),
-    health_condition: Nurse.health_condition(),
-    retry_condition: Nurse.retry_condition(),
-    response_condition: Nurse.response_condition()
-  }
+          check_name: Nurse.name(),
+          check_delay: Nurse.check_delay(),
+          connection_timeout: Nurse.connection_timeout(),
+          evaluation_interval: Nurse.evaluation_interval(),
+          request_method: Nurse.method(),
+          request_body: Nurse.body(),
+          request_header: Nurse.headers(),
+          request_hostname: Nurse.hostname(),
+          request_port: Nurse.eport(),
+          request_scheme: Nurse.scheme(),
+          response_timeout: Nurse.response_timeout(),
+          retry_delay: Nurse.retry_delay(),
+          health_condition: Nurse.health_condition(),
+          retry_condition: Nurse.retry_condition(),
+          response_condition: Nurse.response_condition()
+        }
 
   @spec keys() :: List.t()
   def keys() do
-    [:check_name,
-    :check_delay,
-    :connection_timeout,
-    :evaluation_interval,
-    :request_method,
-    :request_body,
-    :request_header,
-    :request_hostname,
-    :request_port,
-    :request_scheme,
-    :response_timeout,
-    :retry_delay,
-    :health_condition,
-    :retry_condition,
-    :response_condition]
+    [
+      :check_name,
+      :check_delay,
+      :connection_timeout,
+      :evaluation_interval,
+      :request_method,
+      :request_body,
+      :request_header,
+      :request_hostname,
+      :request_port,
+      :request_scheme,
+      :response_timeout,
+      :retry_delay,
+      :health_condition,
+      :retry_condition,
+      :response_condition
+    ]
   end
 
   @spec get(t(), atom()) :: any()
@@ -46,15 +47,15 @@ defmodule NurseWeb.HealthcheckConfig do
     Map.get(map, key, nil)
   end
 
-  @spec from_map( Map.t() ) :: t() | {:missing_key, key()} | {:repeated_key, key()}
-  def from_map( map ) do
-    fold_fun =
-      fn (key , acc) ->
-        key_str = Atom.to_string(key)
-        throw_fun = fn -> throw({:missing_key, key}) end
-        new_val = Map.get_lazy(map, key_str, throw_fun)
-        add(key_str, new_val, acc)
-      end
+  @spec from_map(Map.t()) :: t() | {:missing_key, key()} | {:repeated_key, key()}
+  def from_map(map) do
+    fold_fun = fn key, acc ->
+      key_str = Atom.to_string(key)
+      throw_fun = fn -> throw({:missing_key, key}) end
+      new_val = Map.get_lazy(map, key_str, throw_fun)
+      add(key_str, new_val, acc)
+    end
+
     try do
       List.foldl(keys(), new(), fold_fun)
     catch
@@ -72,5 +73,4 @@ defmodule NurseWeb.HealthcheckConfig do
   defp add(key, value, config) do
     Map.put_new(config, key, value)
   end
-
 end
